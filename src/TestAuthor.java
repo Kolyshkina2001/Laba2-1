3ddb68332236157b36c95fe72479c8f465bde5ea
KARPOVTOP <coolxex86@gmail.com>
2020-09-18 10:34:38 +0300

    Initial commit

---
+++ src/TestAuthor.java
@@ -0,0 +1,8 @@
+public class TestAuthor {
+    public static void main(String[] args){
+        Author a1=new Author("ya.kolysh@gmail.com","Yana Kolyshkina",'w');
+        Author a2=new Author("asta111ken@bk.ru","Tosha Astahov", 'm');
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+    }
+}
